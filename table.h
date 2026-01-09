744ad7f2baf2cda6d792741f5b3d9a8fc6752fac
Rahuldeb5 <rahuldeb.usa@gmail.com>
2026-01-09 00:34:00 -0500

    brainstromed and created intended function prototypes for tables file

---
+++ table.h
@@ -0,0 +1,24 @@
+#ifndef TABLE_H
+#define TABLE_H
+
+#include "entry.h"
+
+int user_table_create(const char* filename);
+int club_table_create(const char* filename);
+
+int user_table_insert(const char* filename, struct user_entry* entry);
+int club_table_insert(const char* filename, struct club_entry* entry);
+
+int user_table_read(const char* filename, int id, struct user_entry* entry);
+int club_table_read(const char* filename, int id, struct club_entry* entry);
+
+int user_table_read_all(const char* filename);
+int club_table_read_all(const char* filename);
+
+int user_table_update(const char* filename, int index, struct user_entry* entry);
+int club_table_update(const char* filename, int index, struct club_entry* entry);
+
+int user_table_delete(const char* filename, int id);
+int club_table_delete(const char* filename, int id);
+
+#endif
